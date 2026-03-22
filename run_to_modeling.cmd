5137d7704232f1ee605bc5615f41945bc493b105
Jean Bezerra <jcbm.contato@outlook.com>
2026-03-22 18:32:59 -0300

    Novo script run_to_modeling.cmd

---
+++ run_to_modeling.cmd
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+python scripts/data-normalization/flights-normalization.py
+python scripts/data-download/script-create-parquet-structure.py
+python scripts/feature-engineering/build_features.py
